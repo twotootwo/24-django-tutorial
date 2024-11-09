79eac8f55fb7e51d73bff836b402736aada0cbbe
Taeho <songtaeho@songtaehoui-MacBookPro.local>
2024-11-09 23:56:08 +0900

    assign2

--- main/views.py
+++ main/views.py
@@ -9,12 +9,25 @@ from rest_framework.mixins import (
     DestroyModelMixin,
 )
 
+from main.models import Student
+from main.serializers import StudentSerializer
+
 
 class StudentListAPIView(ListModelMixin, CreateModelMixin, GenericAPIView):
     """
     GET: 학생 목록 조회
     POST: 학생 추가
     """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    # 4. GET 요청: 학생 목록 조회
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
+
+    # 5. POST 요청: 학생 추가
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
     ### assignment2: 이곳에 과제를 작성해주세요
     ### end assignment2
@@ -28,6 +41,17 @@ class StudentAPIView(
     PATCH: 학생 수정
     DELETE: 학생 삭제
     """
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
 
+    # PATCH 요청 처리
+    def patch(self, request, *args, **kwargs):
+        return self.partial_update(request, *args, **kwargs)
+
+    # DELETE 요청 처리
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
     ### assignment2: 이곳에 과제를 작성해주세요
     ### end assignment2
